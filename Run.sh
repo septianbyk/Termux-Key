b0aefaa0d7e63d1569a027ed2ccc1ea3861d9edc
Kang BaIT <87496336+BAIT-LINUX@users.noreply.github.com>
2021-12-03 22:09:04 +0700

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -13,22 +13,18 @@ echo ""
 echo ""
 echo ""
 echo "\033[5;32m"
-unzip Usr.zip
 
 cd /data/data/com.termux/files/usr/etc
 rm -rf bash.bashrc
 rm -rf motd
-cd /data/data/com.termux/files/home/Termux-Key/Usr/
+cd /data/data/com.termux/files/home/Termux-Key/Lock/Usr/
 mv bash.bashrc /data/data/com.termux/files/usr/etc/
-mv Lock.sh /data/data/com.termux/files/home/
-mv input.sh /data/data/com.termux/files/home/
-pkg install neofetch
+cd  /data/data/com.termux/files/home/Termux-Key/
+mv Lock /data/data/com.termux/files/home/
+pkg install neofetch -y
 cd /data/data/com.termux/files/home/Termux-Key/
-rm -rf Usr.zip
 rm -rf Run.sh
 rm -rf LICENSE
 rm -rf README.md
 rm -rf .git
 rm -rf .gitignore
-cd
-chmod +x Lock.sh
