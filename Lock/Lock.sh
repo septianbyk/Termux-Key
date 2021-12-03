59e9034cc456d378d60345a44c8e3228e8569ad3
Kang BaIT <87496336+BAIT-LINUX@users.noreply.github.com>
2021-12-03 22:03:23 +0700

    Add files via upload

---
+++ Lock/Lock.sh
@@ -0,0 +1,26 @@
+
+clear
+#Masukan Passwordnya Om
+pas=
+echo "\033[5;31m"
+echo ""
+echo ""
+echo "     █░▄▀ █▀▀█ █▀▀▄ █▀▀▀   █▀▀█ █▀▀█ ▀█▀ ▀▀█▀▀        "
+echo "     █▀▄░ █▄▄█ █░░█ █░▀█   █▀▀▄ █▄▄█ ░█░ ░▒█░░        "
+echo "     █░▒█ ▀░░▀ ▀░░▀ ▀▀▀▀   █▄▄█ ▀░░▀ ▄█▄ ░▒█░░        "
+echo "\033[5;33m               ┏━━┓░░░░░░░░░┏┓┏┓░┏┳┓"
+echo "               ┃━━┫┏━┓┏┳┓┏┳┓┣┫┃┗┓┃┃┃"
+echo "               ┣━━┃┃━┫┃┃┃┃┏┛┃┃┃┏┫┣┓┃"
+echo "               ┗━━┛┗━┛┗━┛┗┛░┗┛┗━┛┗━┛"
+echo ""
+echo ""
+echo ""
+echo "\033[5;32m"
+read -p "Masukan Password: " pass
+case $pass in
+#Password Elu Om
+$pas)cd Lock/Usr/
+sh Home.sh;;
+*) cd
+sh input.sh;;
+esac
---
+++ Lock/output.sh
@@ -0,0 +1,22 @@
+clear
+#Masukan Passwordnya Om
+pas=
+echo "\033[5;31m"
+echo ""
+echo ""
+echo "     █░▄▀ █▀▀█ █▀▀▄ █▀▀▀   █▀▀█ █▀▀█ ▀█▀ ▀▀█▀▀        "
+echo "     █▀▄░ █▄▄█ █░░█ █░▀█   █▀▀▄ █▄▄█ ░█░ ░▒█░░        "
+echo "     █░▒█ ▀░░▀ ▀░░▀ ▀▀▀▀   █▄▄█ ▀░░▀ ▄█▄ ░▒█░░        "
+echo "\033[5;33m               ┏━━┓░░░░░░░░░┏┓┏┓░┏┳┓"
+echo "               ┃━━┫┏━┓┏┳┓┏┳┓┣┫┃┗┓┃┃┃"
+echo "               ┣━━┃┃━┫┃┃┃┃┏┛┃┃┃┏┫┣┓┃"
+echo "               ┗━━┛┗━┛┗━┛┗┛░┗┛┗━┛┗━┛"
+echo ""
+echo "\033[1;31m"
+echo "Password Salah"
+echo "\033[5;32m"
+read -p "Masukan Password: " pass
+case $pass in
+$pas)sh Usr/Home.sh;;
+*) sh input.sh ;;
+esac
